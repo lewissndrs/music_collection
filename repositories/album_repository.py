fa283d7323d7cbab386720fbae241ed2129fd8ac
Rebekah Salem-Dalgety <rebekahsalem-dalgety@Rebekahs-MacBook-Pro.local>
2020-09-22 16:34:14 +0100

    first commit

---
+++ repositories/album_repository.py
@@ -0,0 +1,20 @@
+from db.run_sql import run_sql
+from model.album import Album
+from model.artist import Artist
+
+def save(album):
+    sql = 'INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *'
+    values = [album.title, album.genre, album.artist.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
+
+def select_all():
+    albums = []
+    sql = 'SELECT * FROM albums'
+    results = run_sql(sql)
+    for row in results:
+        album = Album(row['title'],row['genre'],row['artist_id'],row['id'])
+        albums.append(album)
+    return albums
